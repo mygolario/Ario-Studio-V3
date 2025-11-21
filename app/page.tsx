307dd77ce5da5291220e5a945ff424cd71e4b4e7
mygolario <ariokaveh85@gmail.com>
2025-11-21 23:54:59 +0330

    Add Services component to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
+import { Services } from "@/components/Services";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       {/* فاصله برای اینکه Hero زیر هدر ثابت قایم نشه */}
       <div className="pt-24 md:pt-28">
         <Hero />
+        <Services />
       </div>
     </main>
   );
