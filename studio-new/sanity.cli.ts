3f908d5c3e61d70c94b19578ed9cc08e99965b5e
mygolario <ariokaveh85@gmail.com>
2025-11-27 11:12:38 +0330

    feat: Enhance Sanity configuration with improved environment variable handling and deployment settings for better project management

---
+++ studio-new/sanity.cli.ts
@@ -0,0 +1,13 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'v3ydinkq',
+    dataset: 'production'
+  },
+  deployment: {
+    appId: 'zdixr81juxq9a3nqci9zzoxc',
+    autoUpdates: true
+  }
+})
+
---
+++ studio-new/sanity.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemas'
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'v3ydinkq'
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+
+export default defineConfig({
+  name: 'studio-new',
+  title: 'Ario Studio - New',
+
+  projectId,
+  dataset,
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
