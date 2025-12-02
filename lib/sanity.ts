3339edb8b023037f90ad1c8d583454e0cbe1c2df
mygolario <ariokaveh85@gmail.com>
2025-12-02 12:59:00 +0330

    chore: update Sanity client configuration to use environment variables
    - Refactored Sanity client configuration to utilize environment variables for project ID, dataset, and API version, enhancing flexibility and security.

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,6 +1,6 @@
 import { createClient } from "next-sanity";
 
-export const client = createClient({
+export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "h9d2960t",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01",
